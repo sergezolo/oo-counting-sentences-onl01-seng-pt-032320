3b2a0988acf3a1ff23f6dffd9aa167b8a95531f0
Sergey Zolotukhin <zolotukhinsereg@gmail.com>
2020-04-11 23:51:43 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -27,8 +27,8 @@ class String
   end
 
   def count_sentences
-    self.split(/[.!?]/).collect do |sent|
-      sent.count
+    self.split(/[.!?]/).collect do |str|
+      str.count
     end
   end
 end
